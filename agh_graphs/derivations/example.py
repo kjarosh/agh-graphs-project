864e0271c08c7a9d5a222bddea438d2cbb944160
Kamil Jarosz <kjarosh256@gmail.com>
2020-12-05 11:38:22 +0100

    Add example derivation

--- agh_graphs/derivations/example.py
+++ agh_graphs/derivations/example.py
@@ -6,25 +6,17 @@ from productions.p2 import P2
 from utils import gen_name
 from visualize import visualize_graph_layer, visualize_graph_3d
 
-productions = [
-    P1(),
-    P2(),
-    # P2(),
-    # P2()
-]
-
 if __name__ == '__main__':
-    layer = 2
     graph = Graph()
     initial_node_name = gen_name()
     graph.add_node(initial_node_name, layer=0, position=(0.5, 0.5), label='E')
 
-    prod_input = [initial_node_name]
-    for production in productions:
-        prod_input = production.apply(graph, prod_input)
+    [i1, i2] = P1().apply(graph, [initial_node_name])
+    [i1_1, i1_2] = P2().apply(graph, [i1])
+    [i2_1, i2_2] = P2().apply(graph, [i2])
 
     visualize_graph_3d(graph)
     pyplot.show()
 
-    visualize_graph_layer(graph, layer)
+    visualize_graph_layer(graph, 2)
     pyplot.show()
